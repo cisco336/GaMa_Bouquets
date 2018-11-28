f33d48372722771e524c1dc4fc3de238f6503b1e
SEIJA002 <cisco336@gmail.com>
2018-11-28 09:27:23 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+
+// Angular Material
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule } from '@angular/material';
+import { HomeComponent } from './home/home.component';
+
+// FontAwesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSpa, faInfoCircle, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faFacebookF, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { FooterComponent } from './footer/footer.component';
+library.add( faTwitter, faFacebookF, faLinkedinIn, faSpa, faInfoCircle, faUser );
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule,
+    MatInputModule, MatFormFieldModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
